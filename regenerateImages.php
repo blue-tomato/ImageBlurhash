80d972d965984aae776ccbf775f44d01b944b2e7
Markus Tiefenbacher <markus.tiefenbacher@gmail.com>
2025-05-20 08:20:37 +0000

    fix download

--- regenerateImages.php
+++ regenerateImages.php
@@ -1,10 +1,11 @@
 <?php
+
 declare(strict_types=1);
+
+namespace ProcessWire;
 
 set_time_limit(0);
 ini_set('max_execution_time', 0);
-
-namespace ProcessWire;
 
 include_once(__DIR__ . "/../../../index.php");
 
@@ -31,7 +32,7 @@ foreach ($fields->find('type=FieldtypeImage,createBlurhash=1') as $field) {
     foreach ($pages->find("{$fieldName}.count>0,check_access=0") as $page) {
         $totalPages++;
         $images = $page->get($fieldName);
-        echo "Seite #{$page->id}: {$images->count()} Bilder\n";
+        echo "Page #{$page->id}: {$images->count()} images\n";
 
         foreach ($images as $image) {
             $totalImages++;
@@ -39,24 +40,23 @@ foreach ($fields->find('type=FieldtypeImage,createBlurhash=1') as $field) {
 
             $files    = $image->getFiles();
             $basename = $image->name;
-            $path     = $files[$basename] ?? reset($files);
+            $file     = $files[$basename] ?? reset($files);
 
             // optional loading image over HTTP for some special internal cases
             // when image does not exist in the filepath
-            if (empty($path) || !is_file($path)) {
+            if (empty($file) || !is_file($file)) {
                 $url  = $image->url;
                 $data = @file_get_contents($url);
                 if (empty($data)) {
-                    echo "Error (HTTP-Download failed)\n";
+                    echo "FAILED (HTTP download)\n";
                     continue;
                 }
-                $tmp = 'data://text/plain;base64,' . base64_encode($data);
-                $path = $tmp;
+                $file = 'data://application/octet-stream;base64,' . base64_encode($data);
             }
 
-            $hash = $blurMod->createBlurhash($image);
+            $hash = $blurMod->createBlurhash($file);
             if ($hash === null) {
-                echo "FEHLER (Generierung fehlgeschlagen)\n";
+                echo "ERROR (hash generation failed)\n";
                 continue;
             }
 
